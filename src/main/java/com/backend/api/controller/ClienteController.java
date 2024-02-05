678c6ec344ce3bc75ced06fa712f528ee6ff4714
RubenMF99 <rumarfo6@gmail.com>
2024-02-05 01:02:39 -0500

    Refactor: Metodos duplicados

--- src/main/java/com/backend/api/controller/ClienteController.java
+++ src/main/java/com/backend/api/controller/ClienteController.java
@@ -66,36 +66,6 @@ public class ClienteController {
         }
     }
 
-    @PostMapping("/logistica/{idProduct}")
-    public ResponseEntity<Object> postLogistica(@RequestBody Logistica logistica, @PathVariable Integer idProduct) {
-        Optional<Tiposproducto> productoOptional = tiposRepositorio.findById(idProduct);
-        if (productoOptional.isPresent()) {
-            Tiposproducto tiposProducto = productoOptional.get();
-            if (logistica.getCantidadproducto() > 10 && tiposProducto.getTransporte().equals("terrestre")) {
-                logistica.setDescuento((logistica.getPrecioenvio() * 0.05));
-                return ResponseEntity.ok(logisticaRepositorio.save(logistica));
-            } else if(logistica.getCantidadproducto() > 10 && tiposProducto.getTransporte().equals("maritimo")){
-                logistica.setDescuento((logistica.getPrecioenvio() * 0.03));
-                return ResponseEntity.ok(logisticaRepositorio.save(logistica));
-            }else{
-                return ResponseEntity.ok(logistica);
-            }
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontró Tiposproducto con el identificador: " + idProduct);
-        }
-    }
-
-    @GetMapping("/logistica")
-    public ResponseEntity<Object> getListLogisticaByIdCliente(@RequestParam Integer idCliente) {
-        try {
-            List<Logistica> guiaEnvio = logisticaRepositorio.findByClienteId(idCliente);
-            return ResponseEntity.ok(guiaEnvio);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al procesar la solicitud");
-        }
-    }
-
     @GetMapping("/admin/cliente")
     public ResponseEntity<Object> getProfile(@RequestHeader("Authorization") String authorizationHeader) {
         try {
--- src/main/java/com/backend/api/model/Logistica.java
+++ src/main/java/com/backend/api/model/Logistica.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Entity
 @Table(name = "logistica")
